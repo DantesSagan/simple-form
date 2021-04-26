7ddc93785d860bb6ee17ffaa96bc8862c611dbd8
Alexsander <mihaleksval@gmail.com>
2021-04-26 17:07:15 +0500

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@components": path.resolve(__dirname, "src", "components"),
+      },
+    },
+  },
   publicPath: process.env.NODE_ENV === "production" ? "/simple-form/" : "/",
 };
