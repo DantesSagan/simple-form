10f49c4089f1b16b45705b31658d640e4f532a50
Alexsander <mihaleksval@gmail.com>
2021-04-26 13:42:12 +0500

    vue deploy files

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="h-100">
+    <div class="container-fluid h-100 justify-content-center align-items-center">
+      <div class="row h-100">
+        <div class="col-md-1 vue-bg h-100 d-flex"></div>
+        <div class="col-md-9 h-100 d-flex justify-content-center align-items-center">
+          <div class="col-md-8 rounded px-5 py-4 shadow bg-white text-left">
+            <SignupForm />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import SignupForm from "./components/SignupForm.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SignupForm,
+  },
+};
 </script>
 
 <style>
@@ -24,5 +31,41 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin-bottom: 60px;
+  margin-left: 150px;
+}
+.vue-bg {
+  background: #bce5d0;
+}
+@media only screen and (max-width: 1200px) {
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    margin-bottom: 60px;
+    margin-left: 0;
+  }
+  .vue-bg {
+    background: #bce5d0;
+  }
+}
+@media only screen and (max-width: 375px) {
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 30px;
+    margin-bottom: 30px;
+    margin-left: 0;
+    
+  }
+  .vue-bg {
+    background: #bce5d0;
+  }
 }
 </style>
